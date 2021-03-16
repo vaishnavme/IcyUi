e788d1feda88c64b15acd812d4ecc814986c06c6
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-03-16 15:15:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sevenUi
+ 
---
+++ js/app.js
@@ -0,0 +1,11 @@
+const toggleButton = document.querySelector("#sidebarCollapse")
+const customMenu = document.querySelector(".custom-menu")
+
+const sidebar = document.querySelector("#sidebar");
+
+function toggleSideBar() {
+    sidebar.classList.toggle("active")
+    customMenu.classList.toggle("right-move")
+}
+
+toggleButton.addEventListener("click", toggleSideBar)
