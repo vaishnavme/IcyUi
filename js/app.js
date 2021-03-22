8eb66cf6d432676da9db85e8a9561daba2149d6a
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-03-22 22:53:19 +0530

    Feat: onscroll section link active feature

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Responsive sidebar
 const toggleButton = document.getElementById("toggle-button");
 const toggleContent = document.getElementById("sidebar-content");
 
@@ -5,3 +6,24 @@ toggleButton.addEventListener("click", () => {
 	document.body.classList.toggle("active");
 	toggleContent.classList.toggle("active");
 });
+
+// Active section link
+let mainNavLinks = document.querySelectorAll("#sidebar-content li a");
+let mainSection = document.querySelectorAll("main section")
+console.log(mainSection)
+
+let lastId;
+
+window.addEventListener("scroll", event => {
+	let fromTop = window.scrollY;
+
+	mainNavLinks.forEach(link => {
+		let section = document.querySelector(link.hash);
+		console.log(link);
+		if(section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+			link.classList.add("current");
+		} else {
+			link.classList.remove("current");
+		}
+	})
+})
