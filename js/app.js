e62ac7bd886489f8db011792554e036665466868
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-03-25 08:39:51 +0530

    Rename css class and bug fix

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Responsive sidebar
-const toggleButton = document.getElementById("toggle-button");
-const toggleContent = document.getElementById("sidebar-content");
+const toggleButton = document.getElementById("toggle__button");
+const toggleContent = document.getElementById("sidebar__content");
 
 toggleButton.addEventListener("click", () => {
 	document.body.classList.toggle("active");
@@ -8,7 +8,7 @@ toggleButton.addEventListener("click", () => {
 });
 
 // Active section link
-let mainNavLinks = document.querySelectorAll("#sidebar-content li a");
+let mainNavLinks = document.querySelectorAll("#sidebar__content li a");
 let mainSection = document.querySelectorAll("main section")
 
 window.addEventListener("scroll", event => {
@@ -18,9 +18,9 @@ window.addEventListener("scroll", event => {
 		let section = document.querySelector(link.hash);
 
 		if(section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
-			link.classList.add("current");
+			link.classList.add("active");
 		} else {
-			link.classList.remove("current");
+			link.classList.remove("active");
 		}
 	})
 })
